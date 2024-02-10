dd6a29375804dbaad8da456b485fdc18243a870e
noob04coder <78252850+noob04coder@users.noreply.github.com>
2024-02-10 17:42:42 +0530

    Add files via upload

---
+++ delayCode.js
@@ -0,0 +1,3 @@
+console.log(document.getElementsByClassName("coin-marquee-container__inner").item(0))
+// marqDur=parseFloat(document.getElementsByClassName("coin-marquee-container__inner")[0].style.animationDuration)*3;
+// document.getElementsByClassName("coin-marquee-container__inner")[0].style.animationDuration=(marqDur+"s");
